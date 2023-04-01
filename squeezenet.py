882c29b7df191d82a7dd3e9080865291a2ded3e2
zdangz <128911317+zdangz@users.noreply.github.com>
2023-04-01 23:42:31 +0800

    Update squeezenet.py
    Replaced Softmax with ReLU after final conv, and added Dropout(p=0.5)

--- squeezenet.py
+++ squeezenet.py
@@ -7,22 +7,36 @@ class Fire(nn.Module):
   def __init__(self, in_planes, squeeze1x1, expand1x1, expand3x3):
     super().__init__()
     self.squeeze = nn.Conv2d(in_planes, squeeze1x1, kernel_size = 1)
+    self.bn1 = nn.BatchNorm2d(squeeze1x1)
+    self.relu1 = nn.ReLU(inplace=True)                
     self.expand1 = nn.Conv2d(squeeze1x1, expand1x1, kernel_size = 1)
+    self.bn2 = nn.BatchNorm2d(expand1x1)
     self.expand3 = nn.Conv2d(squeeze1x1, expand3x3, kernel_size = 3, padding = 1)
-    for m in self.modules():
-      if isinstance(m, nn.Conv2d):
-        n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
-        m.weight.data.normal_(0, math.sqrt(2./n))
+    self.bn3 = nn.BatchNorm2d(expand3x3)
+    self.relu2 = nn.ReLU(inplace=True)
+    #for m in self.modules():
+    #  if isinstance(m, nn.Conv2d):
+    #    n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+    #    m.weight.data.normal_(0, math.sqrt(2./n))
   
   def forward(self, x):
-    out = F.relu(self.squeeze(x))
-    out = torch.cat([F.relu(self.expand1(out)),F.relu(self.expand3(out))],1)
+    out = self.squeeze(x)
+    out = self.bn1(out)
+    out = self.relu1(out)
+    out1 = self.expand1(out)
+    out1 = self.bn2(out1)
+    out2 = self.expand3(out)
+    out2 = self.bn3(out2)
+    out = torch.cat([out1, out2], 1)
+    out = self.relu2(out)
     return out
   
 class SqueezeNet(nn.Module):
   def __init__(self):
     super(SqueezeNet, self).__init__()
     self.conv1 = nn.Conv2d(3, 96, kernel_size=3, stride=1, padding=1)
+    self.bn1 = nn.BatchNorm2d(96)
+    self.relu = nn.ReLU(inplace=True)
     self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2) #16x16
     self.fire2 = Fire(96, 16, 64, 64)
     self.fire3 = Fire(128, 16, 64, 64)
@@ -35,19 +49,19 @@ class SqueezeNet(nn.Module):
     self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2) #4x4
     self.fire9 = Fire(512, 64, 256, 256)
     self.conv2 = nn.Conv2d(512, 100, kernel_size=1, stride=1)
-    self.avg_pool = nn.AvgPool2d(kernel_size=4, stride=4) #1x1
-    self.softmax = nn.LogSoftmax(dim=1)
-
-    for m in self.modules():
-      if isinstance(m, nn.Conv2d):
-        n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
-        m.weight.data.normal_(0, math.sqrt(2. / n))
-      elif isinstance(m, nn.BatchNorm2d):
-          m.weight.data.fill_(1)
-          m.bias.data.zero_()
+    self.classifier = nn.Sequential(nn.Dropout(p=0.5), self.conv2, nn.ReLU(inplace=True), nn.AdaptiveAvgPool2d((1, 1)))
+    #for m in self.modules():
+    #  if isinstance(m, nn.Conv2d):
+    #    n = m.kernel_size[0] * m.kernel_size[1] * m.in_channels
+    #    m.weight.data.normal_(0, math.sqrt(2. / n))
+    #  elif isinstance(m, nn.BatchNorm2d):
+    #      m.weight.data.fill_(1)
+    #      m.bias.data.zero_()
   
   def forward(self, x):
-    out = F.relu(self.conv1(x))
+    out = self.conv1(x)
+    out = self.bn1(out)
+    out = self.relu(out)
     out = self.maxpool1(out)
     out = self.fire2(out)
     out = self.fire3(out)
@@ -59,9 +73,7 @@ class SqueezeNet(nn.Module):
     out = self.fire8(out)
     out = self.maxpool3(out)
     out = self.fire9(out)
-    out = self.conv2(out)
-    out = self.avg_pool(out)
-    out = self.softmax(out)
+    out = self.classifier(out)
     out = torch.flatten(out, 1) # flatten to [128, 100]
+    #out = self.softmax(out)
     return out
-  
