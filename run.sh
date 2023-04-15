5a077ea26d18bbe9b5bf0da792b57e4d660f4300
rgan19 <rachel_gan@mymail.sutd.edu.sg>
2023-04-15 16:37:18 +0800

    update run learning rate

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ python main.py \
 --dataset_dir ./datasets \
 --batch_size 128 \
 --epochs 100 \
---lr 0.05 --wd 0.0005 \
+--lr 0.12 --wd 0.0005 \
 --lr_scheduler \
 --seed 0 \
---fig_name lr=0.05-e=100-bc.png \
+--fig_name lr=0.12-e=100-base.png \
 --save_images \
 --test \
 --plot
